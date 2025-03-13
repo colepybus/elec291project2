59f0ab438ae96d3c23898aa3189fd491704219a3
Cole Pybus <colepybus@gmail.com>
2025-03-13 16:06:19 -0700

    made changes to makefile for stm32 slave to work with cole mac

--- STM32L051_JDY40_Test/STM32_JDY40_test_Slave/sflash.bat
+++ STM32L051_JDY40_Test/STM32_JDY40_test_Slave/sflash.bat
@@ -1,2 +1,1 @@
-..\stm32flash\stm32flash -w main.hex -v -g 0x0 ^
-COM13
+..stm32flashstm32flash -w main.hex -v -g 0x0 ^^
---
+++ STM32L051_JDY40_Test/STM32_JDY40_test_Slave/sflash.sh
@@ -0,0 +1,2 @@
+../stm32flash/stm32flash -w main.hex -v -g 0x0 /dev/cu.usbserial-D30HJN8C
+../stm32flash/BO230/BO230 -b

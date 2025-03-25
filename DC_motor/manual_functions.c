c7c8d5c7b1260b061a115cb18602c23e14059a08
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2025-03-25 15:46:57 -0700

    Update manual_functions.c

--- DC_motor/manual_functions.c
+++ DC_motor/manual_functions.c
@@ -1,3 +1,24 @@
+/* Functions for auto mode motor control, NOT FINISHED  
+ * ELEC 291 Project 2
+ * Group B01
+ * Created March 25th, 2024 by Madison Howitt and Ria Dangi
+ */
+
+#include "../Common/Include/stm32l051xx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Common/Include/serial.h"
+#include <time.h>
+
+#define F_CPU 32000000L // 32MHz operating frequency 
+#define DEF_F 100000L   // 10µs tick for timer
+#define PWM_MAX 100     // max PWM speed 
+
+volatile int PWM_Counter = 0;
+volatile unsigned char pwm1 = 100, pwm2 = 100;
+
+
+
 void turn_right_man ()
 {
     pwm1 = 100;
